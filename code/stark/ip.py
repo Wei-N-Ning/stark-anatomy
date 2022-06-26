2e0a6b6ac35bf38a3af5e8822d669b3ccedd4968
Wei-N-Ning <macgnw@gmail.com>
2022-06-26 17:49:10 +1000

    rewrite proof stream unit tests

--- code/stark/ip.py
+++ code/stark/ip.py
@@ -1,5 +1,6 @@
-from hashlib import shake_256
 import pickle as pickle  # serialization
+from hashlib import shake_256
+from typing import Any
 
 
 class ProofStream:
@@ -8,27 +9,28 @@ class ProofStream:
         self.objects = []
         self.read_index = 0
 
-    def push(self, obj):
+    def push(self, obj: Any):
         self.objects += [obj]
 
-    def pull(self):
+    def pull(self) -> Any:
         assert (self.read_index < len(
             self.objects)), "ProofStream: cannot pull object; queue empty."
         obj = self.objects[self.read_index]
         self.read_index += 1
         return obj
 
-    def serialize(self):
+    def serialize(self) -> bytes:
         return pickle.dumps(self.objects)
 
-    def prover_fiat_shamir(self, num_bytes=32):
+    def prover_fiat_shamir(self, num_bytes=32) -> bytes:
         return shake_256(self.serialize()).digest(num_bytes)
 
-    def verifier_fiat_shamir(self, num_bytes=32):
+    def verifier_fiat_shamir(self, num_bytes=32) -> bytes:
         return shake_256(pickle.dumps(
             self.objects[:self.read_index])).digest(num_bytes)
 
-    def deserialize(self, bb):
+    @staticmethod
+    def deserialize(bb) -> 'ProofStream':
         ps = ProofStream()
         ps.objects = pickle.loads(bb)
         return ps
